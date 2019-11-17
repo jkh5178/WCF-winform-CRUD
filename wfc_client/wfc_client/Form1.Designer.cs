1e6555131025e5283936095e283dc95379db3228
jkh5178 <jkh5178@naver.com>
2019-11-17 16:21:39 +0900

    Initial commit

---
+++ wfc_client/wfc_client/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace wfc_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_remove = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(582, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(653, 20);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(169, 44);
+            this.btn_read.TabIndex = 1;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(653, 100);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(169, 44);
+            this.btn_create.TabIndex = 2;
+            this.btn_create.Text = "Create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(653, 180);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(169, 44);
+            this.btn_update.TabIndex = 3;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(653, 260);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(169, 44);
+            this.btn_remove.TabIndex = 4;
+            this.btn_remove.Text = "Remove";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(851, 322);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_remove;
+    }
+}
+
---
+++ wfc_client/wfc_client/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace wfc_client
+{
+    public partial class Form1 : Form
+    {
+        public ServiceReference2.Service1Client sc = new ServiceReference2.Service1Client();
+        private DataSet Data = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            try {
+                
+                Data = sc.readDB();
+                dataGridView1.DataSource = Data.Tables["Databases"];
+            }
+            catch
+            {
+                MessageBox.Show("연결확인");
+            }
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            try {
+                String[] st = new String[5];
+                DataTable dtChanged = Data.Tables["Databases"].GetChanges(DataRowState.Added);
+                foreach (DataRow row in dtChanged.Rows)
+                {
+                    if (row.RowState == DataRowState.Added)
+                    {
+                       for(int i = 0; i < 5; i++)
+                        {
+                            st[i] = row[i].ToString();
+                        }
+                        sc.createDB(st);
+                    }
+
+                }
+
+            }
+            catch {
+                MessageBox.Show("연결확인");
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try {
+                sc.updateDB(Data.Tables["Databases"].GetChanges(DataRowState.Modified));
+
+            }
+            catch {
+                MessageBox.Show("연결확인");
+            }
+        }
+
+        private void btn_remove_Click(object sender, EventArgs e)
+        {
+            try {
+                sc.removeDB(dataGridView1.CurrentRow.Cells[2].Value.ToString());
+                Data.Tables["Databases"].Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+            }
+            catch {
+                MessageBox.Show("연결확인");
+            }
+        }
+    }
+}
